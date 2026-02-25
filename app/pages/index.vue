41f1d3dc0e3bca65fc771543aa387b403e98808a
TrojanSun <yashuiqingshan@gmail.com>
2026-02-25 22:25:11 +0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,10 +2,15 @@
 definePageMeta({
   layout: false,
 })
+useHead({                                                                                                                                                                                                                                                                   
+    title: 'DWZ',                                                                                                                                                                                                                                                             
+}) 
 </script>
 
 <template>
   <div class="flex min-h-screen items-center justify-center">
-    dwz
+    <p>dwz.si</p>
+    <p>dwz.lu</p>
+    <p>dwz.fi</p>
   </div>
 </template>
